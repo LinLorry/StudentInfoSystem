73796aba83152df0cfcf7e06b8a0646317c9ad52
LinLorry <lin864464995@163.com>
2020-05-26 17:33:46 +0800

    :ok_hand: Update use perror.

--- src/student_info_system/init.c
+++ src/student_info_system/init.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <errno.h>
 
 #include <student_info_system/base.h>
 #include <student_info_system/init.h>
@@ -22,8 +23,8 @@ int init()
 
     if ((fp = fopen(DATA_TMP_FILE_NAME, "wb")) == NULL)
     {
-        fprintf(stderr, "Create tmp file failed!\n"
-                        "Initial student info system failed.\n"
+        perror("Create tmp file failed: ");
+        fprintf(stderr, "Initial student info system failed.\n"
                         "Exit.\n");
         exit(EXIT_FAILURE);
     }
@@ -69,8 +70,8 @@ int init()
 
     if (fclose(fp))
     {
-        fprintf(stderr, "Close tmp file error\n"
-                        "Initial student info system failed.\n"
+        perror("Close tmp file error: ");
+        fprintf(stderr, "Initial student info system failed.\n"
                         "Exit.\n");
         exit(EXIT_FAILURE);
     }
@@ -78,8 +79,8 @@ int init()
     printf("Save data tmp file success.\n");
     if (rename(DATA_TMP_FILE_NAME, DATA_FILE_NAME))
     {
-        fprintf(stderr, "Rename data tmp file to data file failed\n"
-                        "Initial student info system failed.\n"
+        perror("Rename data tmp file to data file failed: ");
+        fprintf(stderr, "Initial student info system failed.\n"
                         "Exit.\n");
         exit(EXIT_FAILURE);
     }
