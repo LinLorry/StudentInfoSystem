40f239b7c1ae125a75562e8d43b73b1ea16deeba
LinLorry <lin864464995@163.com>
2020-06-01 14:36:05 +0800

    :sparkles: Add update username and change password view.

--- src/user_view.c
+++ src/user_view.c
@@ -13,6 +13,10 @@ int show_user();
 
 int create_user();
 
+int update_user_view();
+
+int change_password_view();
+
 int remove_user();
 
 int user_manage()
@@ -20,12 +24,14 @@ int user_manage()
     const menu menus[] = {
         {"show users", show_user},
         {"create user", create_user},
+        {"update user", update_user_view},
+        {"change password", change_password_view},
         {"remove user", remove_user}
     };
 
     if (ADMIN_LEVEL_VALUE == get_current_level())
     {
-        return show_menu(menus, 3, NULL);
+        return show_menu(menus, 5, NULL);
     }
     else
     {
@@ -88,6 +94,106 @@ int create_user()
 
     printf("Press any key continue.");
     CLEAR_STDIN();
+
+    return 0;
+}
+
+int update_user_view()
+{
+    int tmp;
+    unsigned long id;
+    char tmp_str[256];
+    unsigned char level;
+    char username[USERNAME_BUFFER_SIZE];
+    const_user_info *user_info_p;
+
+    CLEAR();
+    print_users();
+
+    printf("Please input user id which you want to update: ");
+    tmp = scanf("%ld", &id);
+    CLEAR_STDIN();
+
+    while (tmp != 1 || id < 1)
+    {
+        printf("Please input valid id: ");
+
+        tmp = scanf("%ld", &id);
+        CLEAR_STDIN();
+    }
+    user_info_p = get_user(id);
+    level = user_info_p->level;
+
+    printf(
+        "User original info:\nlevel: %s\nusername: %s\n",
+        level == ADMIN_LEVEL_VALUE ? admin_str : user_str,
+        user_info_p->username);
+
+    if (level == ADMIN_LEVEL_VALUE)
+    {
+        printf("Change this user type to user[y/N]?");
+        tmp = getchar();
+        CLEAR_STDIN_WITH_PRE(tmp);
+        if (tmp == 'y' || tmp == 'Y')
+        {
+            level = USER_LEVEL_VALUE;
+        }
+    }
+    else
+    {
+        printf("Change this user type to admin[y/N]?");
+        tmp = getchar();
+        CLEAR_STDIN_WITH_PRE(tmp);
+        if (tmp == 'y' || tmp == 'Y')
+        {
+            level = ADMIN_LEVEL_VALUE;
+        }
+    }
+
+    sprintf(tmp_str, "Username length must less then %d\n", USERNAME_MAX_LENGTH);
+    input_str(
+        username, USERNAME_MAX_LENGTH,
+        "Please input new username: ",
+        "Please input new username again: ",
+        tmp_str
+    );
+
+    tmp = update_user(id, level, username, NULL);
+
+    return 0;
+}
+
+int change_password_view()
+{
+    int tmp;
+    unsigned long id;
+    char tmp_str[256];
+    char password[PASSWORD_BUFFER_SIZE];
+
+    CLEAR();
+    print_users();
+
+    printf("Please input user id which you want to update: ");
+    tmp = scanf("%ld", &id);
+    CLEAR_STDIN();
+
+    while (tmp != 1 || id < 1)
+    {
+        printf("Please input valid id: ");
+
+        tmp = scanf("%ld", &id);
+        CLEAR_STDIN();
+    }
+
+    sprintf(tmp_str, "Password length must less then %d\n", PASSWORD_MAX_LENGTH);
+    input_str(
+        password, PASSWORD_MAX_LENGTH,
+        "Please input new password: ",
+        "Please input new password again: ",
+        tmp_str
+    );
+
+    tmp = update_user(id, NULL_LEVEL_VALUE, NULL, password);
 
     return 0;
 }
